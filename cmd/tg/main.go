90f7fd73e14376d593eb83bdf452c2f0fdcae540
Daniel Lewan <vision360.daniel@gmail.com>
2021-07-25 19:51:44 +0200

    tg: fix space after last attribute

--- cmd/tg/main.go
+++ cmd/tg/main.go
@@ -19,28 +19,38 @@ func main() {
 
 	fi, _ := os.Stdin.Stat()
 	if (fi.Mode() & os.ModeCharDevice) == 0 {
+		// stdin is pipe
 		io.Copy(b, os.Stdin)
 	}
 
+	// write tag
 	b.WriteRune('<')
 	b.WriteString(tag)
 
-	if len(*attrs) > 0 {
+	// write attributes
+	attrsCount := len(*attrs)
+	if attrsCount > 0 {
 		b.WriteString(" ")
-		for _, a := range *attrs {
+		for i, a := range *attrs {
 			if strings.Contains(a, "=") {
 				attr := strings.Split(a, "=")
 				fmt.Fprintf(b, `%s="%s"`, attr[0], attr[1])
 			} else {
 				b.WriteString(a)
 			}
-			b.WriteRune(' ')
+
+			if i < attrsCount-1 {
+				b.WriteRune(' ')
+			}
 		}
 	}
 
 	if len(pflag.Args()) > 1 {
 		b.WriteRune('>')
+
+		// write children
 		b.WriteString(strings.Join(pflag.Args()[1:], " "))
+
 		fmt.Fprintf(b, "<%s/>", tag)
 	} else {
 		b.WriteString("/>")
