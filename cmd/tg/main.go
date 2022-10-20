59ead99a976f2fa268ba0c7c05646a36d3bc6e45
Daniel Lewan <vision360.daniel@gmail.com>
2022-10-20 23:00:10 +0200

    tg: basic HTML5 template and fix tags

--- cmd/tg/main.go
+++ cmd/tg/main.go
@@ -10,50 +10,77 @@ import (
 	"github.com/yosssi/gohtml"
 )
 
-func main() {
-	attrs := pflag.StringArrayP("attribute", "a", nil, "list of attributes for the tag")
-	pflag.Parse()
+var basic = `<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>TITLE</title>
+</head>
+<body>
+`
 
-	tag := pflag.Arg(0)
-	b := &strings.Builder{}
+var basicEnd = `
+</body>
+</html>`
 
-	fi, _ := os.Stdin.Stat()
-	if (fi.Mode() & os.ModeCharDevice) == 0 {
-		// stdin is pipe
-		io.Copy(b, os.Stdin)
-	}
-
+func writeTag(b *strings.Builder, tag string, attrs, children []string) {
 	// write tag
 	b.WriteRune('<')
 	b.WriteString(tag)
 
 	// write attributes
-	attrsCount := len(*attrs)
+	attrsCount := len(attrs)
 	if attrsCount > 0 {
 		b.WriteString(" ")
-		for i, a := range *attrs {
+		for i, a := range attrs {
 			if strings.Contains(a, "=") {
 				attr := strings.Split(a, "=")
 				fmt.Fprintf(b, `%s="%s"`, attr[0], attr[1])
 			} else {
 				b.WriteString(a)
 			}
-
 			if i < attrsCount-1 {
 				b.WriteRune(' ')
 			}
 		}
 	}
 
-	if len(pflag.Args()) > 1 {
+	if len(children) > 0 {
 		b.WriteRune('>')
-
 		// write children
-		b.WriteString(strings.Join(pflag.Args()[1:], " "))
+		b.WriteString(strings.Join(children, " "))
 
-		fmt.Fprintf(b, "<%s/>", tag)
+		fmt.Fprintf(b, "</%s>", tag)
 	} else {
 		b.WriteString("/>")
+	}
+}
+
+func copyStdin(b *strings.Builder) {
+	fi, _ := os.Stdin.Stat()
+	if (fi.Mode() & os.ModeCharDevice) == 0 {
+		// stdin is pipe
+		io.Copy(b, os.Stdin)
+	}
+}
+
+func main() {
+	attrs := pflag.StringArrayP("attribute", "a", nil, "list of attributes for the tag")
+	tpl := pflag.BoolP("basic", "b", false, "basic HTML template")
+	title := pflag.StringP("title", "t", "Document", "title for basic template")
+	pflag.Parse()
+
+	tag := pflag.Arg(0)
+	b := &strings.Builder{}
+
+	if !*tpl {
+		copyStdin(b)
+		writeTag(b, tag, *attrs, pflag.Args()[1:])
+	} else {
+		b.WriteString(strings.Replace(basic, "TITLE", *title, 1))
+		copyStdin(b)
+		b.WriteString(basicEnd)
 	}
 
 	fmt.Println(gohtml.Format(b.String()))
